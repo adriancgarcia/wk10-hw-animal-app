cdfa97f9d111a986ffda5bc32af59ff6c55dc2d5
adriancgarcia <adriancgarcia@gmail.com>
2023-06-28 15:01:07 -0500

    connected to server.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+require("dotenv").config
 const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require ("method-override");
+
 
 const app = express();
+const PORT = process.env.PORT
+
+// MIDDLEWARE /////
+app.use(morgan("dev"))
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({extended: true}))
+app.use("/static", express.static("public"))
+
+
+// ROUTES ///////
+app.get("/", (req, res) => {
+    res.send("server is working")
+})
+  
 
 
 
-
-
-
-app.listen(3000, () => {
+app.listen(3003, () => {
     console.log("listening on port, 3003")
 })
